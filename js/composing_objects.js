522cbb8707edebcb2f1df34d583982ed667d1865
elbie-em <emoonga1993@gmail.com>
2020-07-07 19:47:04 +0200

    building composites: class inheritance & mixin composition

--- js/composing_objects.js
+++ js/composing_objects.js
@@ -8,3 +8,37 @@ const fullName = {
 };
 
 console.log(fullName);
+
+//Building composites with class inheritance
+
+class Foo {
+  constructor(){
+    this.a = 'a'
+  }
+}
+
+class Bar extends Foo {
+  constructor() {
+    super();
+    this.b = 'b';
+  }
+}
+
+const myBar = new Bar();
+
+console.log(myBar);
+
+//Building composites with mixin composition
+
+const a = {
+   a: 'a'
+};
+
+const b = {
+  b: 'b',
+  f: 'f'
+};
+
+const c = {...a,...b};
+
+console.log(c);
