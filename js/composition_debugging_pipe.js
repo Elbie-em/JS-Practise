368d2401f6d38046f1b8d5387a55a18015c11ac5
elbie-em <emoonga1993@gmail.com>
2020-07-07 18:39:00 +0200

    using pipe to counter compostion with debugging

---
+++ js/composition_debugging_pipe.js
@@ -0,0 +1,15 @@
+//Using a pipe to counter composition with debugging
+
+const g = n => n + 1;
+const f = n => n * 2;
+
+const trace = label => value => {
+  console.log(`${label}: ${value}`);
+  return value
+};
+
+const pipe = (...fns) => x => fns.reduce((y,f) => f(y), x);
+
+const doStuffBetter = pipe(g, trace('after g'),f,trace('after f'));
+
+doStuffBetter(20);
