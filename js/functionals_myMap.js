57febdb073c6fad03009b6d7ae907c5d9e4eec3d
elbie-em <emoonga1993@gmail.com>
2020-07-10 01:16:04 +0200

    creating my own mymap method

---
+++ js/functionals_myMap.js
@@ -0,0 +1,20 @@
+//My own map method
+
+// the global Array
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback){
+  let newArray = [];
+  //this.forEach(a => newArray.push(callback(a)));
+  for(let i = 0; i < this.length; i++){
+    newArray.push(callback(this[i]));
+  }
+
+  return newArray;
+};
+
+var new_s = s.myMap(item => {
+  return item * 2;
+});
+
+console.log(new_s)
