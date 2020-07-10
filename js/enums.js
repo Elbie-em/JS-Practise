1b90aa5fcb66aaf5e56c0d79292f188337c46212
elbie-em <emoonga1993@gmail.com>
2020-07-10 21:36:58 +0200

    a slice in a slice changed to arrow functions

--- js/enums.js
+++ js/enums.js
@@ -1,6 +1,6 @@
 ///Customn slice
 
-function sliceArray(anim, beginSlice, endSlice) {
+const sliceArray = (anim, beginSlice, endSlice) => {
 
   return anim.slice(beginSlice,endSlice)
 
@@ -8,4 +8,15 @@ function sliceArray(anim, beginSlice, endSlice) {
 const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 const sliced = sliceArray(inputAnim, 1, 3);
 
-console.log(sliced);
+//console.log(sliced);
+
+//Replacing splice with slice
+const nonMutatingSplice = (cities) => {
+ 
+  return cities.slice(0,3);
+
+  
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+console.log(nonMutatingSplice(inputCities));
