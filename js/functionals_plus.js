f57831fe05749a50c495160afa2572f398264437
elbie-em <emoonga1993@gmail.com>
2020-07-12 00:10:14 +0200

    using some and every

---
+++ js/functionals_plus.js
@@ -0,0 +1,32 @@
+// Every at Work
+
+var numbers = [1, 5, 8, 0, 7, 9];
+
+const isLessThan10 = numbers.every((currentValue) => {
+  return currentValue < 10;
+});
+
+console.log(isLessThan10);
+
+//Check if every number is a positive
+
+const checkPositive = (arr) => {
+  const isPositive = arr.every((currentValue) => {
+    return currentValue > 0;
+  });
+
+  return isPositive;
+}
+
+console.log(checkPositive([1, 2, 3, -4, 5]));
+
+//Check if every number is a positive
+const checkSomePositive = (arr) => {
+  const isPositive = arr.some((currentValue) => {
+    return currentValue > 0;
+  });
+
+  return isPositive;
+}
+
+console.log(checkSomePositive([1, 2, 3, -4, 5]));
